df540b585dad085257cdf64c2b7acc1ec8cfa847
geonh22 <geonh22@naver.com>
2018-12-06 21:04:14 +0900

    facebook login

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,19 @@ module.exports = (app, passport) => {
       res.redirect('/questions');
     }
   );
+  app.get('/auth/naver',
+    passport.authenticate('naver', { scope : 'email' })
+  );
 
+  app.get('/auth/naver/callback',
+    passport.authenticate('naver', {
+      failureRedirect : '/signin',
+      failureFlash : true // allow flash messages
+    }), (req, res, next) => {
+      req.flash('success', 'Welcome!');
+      res.redirect('/questions');
+    }
+  )
   app.get('/signout', (req, res) => {
     req.logout();
     req.flash('success', 'Successfully signed out');
